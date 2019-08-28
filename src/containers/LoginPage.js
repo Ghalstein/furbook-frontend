310e0704b0db79186d0e19c644f0c0d7411522df
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-28 19:32:59 -0400

    full auth

---
+++ src/containers/LoginPage.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class LoginPage extends React.Component{
+
+	state = {
+		username: "",
+		password: ""
+	}
+
+	handleChange = (event) => {
+		this.setState({[event.target.name]: event.target.value})
+		// console.log(this.state);
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+		fetch('http://localhost:3000/login', {
+			method: "POST",
+			headers: {
+				'Content-Type': 'application/json',
+				'Accept': 'application/json'
+			},
+			body: JSON.stringify(this.state)
+		})
+		.then(res => res.json())
+		.then(resp => {
+			console.log(resp)
+			localStorage.setItem('token', resp.token)
+			this.props.history.push('/home')
+		});
+	}
+
+  render = () => {
+  	if (localStorage.token) this.props.history.push("/home")
+    return (
+      <form className="loginForm" onSubmit={this.handleSubmit}>
+        <input className="loginInput" type="text" value={this.state.username} onChange={this.handleChange} name="username"/>
+        <input className="loginInput" type="password" value={this.state.password} onChange={this.handleChange} name="password"/>
+        <input className="loginSubmit" type="submit" value="Login"/>
+      </form>
+    );
+  }
+}
+
+export default LoginPage;
