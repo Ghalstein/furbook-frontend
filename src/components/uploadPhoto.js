a7cc8ceda4b16fdabda181af0f5b686c4ecc3a03
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-31 14:54:25 -0400

    refactored uplaod button

--- src/components/uploadPhoto.js
+++ src/components/uploadPhoto.js
@@ -21,23 +21,6 @@ export default class uploadPhoto extends React.Component {
 	    data: formData,
 	    config: { headers: {'Content-Type': 'multipart/form-data', Authorization: localStorage.token }}
 	  });
-		// fetch('http://localhost:3000/photos', {
-		// 	method: 'POST',
-		// 	headers: {
-		// 		'Content-Type': 'application/json',
-		// 		'Accept': 'application/json',
-		// 		'Authorization': localStorage.token
-		// 	},
-		// 	body: JSON.stringify(formData)
-		// }).then(res => res.json())
-		// 	.then(console.log)
-		// $.ajax({
-		// 	url:'/photos',
-		// 	method: 'POST',
-		// 	data: formData,
-		// 	contentType: false,
-		// 	processData: false
-		// })
 	}
 
 	handleFile = (e) => {
@@ -47,10 +30,13 @@ export default class uploadPhoto extends React.Component {
 	render() {
 		console.log(this.state);
 		return (
-			<form onSubmit={this.handleSubmit}>
-				<input type="file" onChange={this.handleFile}/>
-				<button>Upload Photo </button>
-			</form>
+			<div>
+				Upload a new photo
+				<form onSubmit={this.handleSubmit}>
+					<input type="file" onChange={this.handleFile}/>
+					<button>Upload Photo </button>
+				</form>
+			</div>
 		);
 	}
 }
