e5f332cb401ea06e322c1581cc3c8b3d9a685ba2
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-31 17:36:21 -0400

    Home page is up to date

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -3,21 +3,10 @@ import HomeHeader from '../components/HomeHeader';
 
 class HomePage extends React.Component {
 
-	// componentDidMount = () => {
-	// 	fetch('http://localhost:3000/profile', {
-	// 		headers: {
-	// 			Authorization: localStorage.token
-	// 		}
-	// 	})
-	// 	.then(res => res.json())
-	// 	.then(profileInfo => {
-	// 		console.log(profileInfo)
-	// 		this.setState({ username: profileInfo.username })})
-	// }
 
   render = () => {
-  	if (!localStorage.token) this.props.history.push("/")
-  		console.log(this.props)
+  	if ((!localStorage.token) && this.props.hasOwnProperty('history')) this.props.history.push("/")
+  		console.log(this.props.userInfo)
     return (
       <div className="HomePage">
         <HomeHeader userInfo={this.props.userInfo}/>
