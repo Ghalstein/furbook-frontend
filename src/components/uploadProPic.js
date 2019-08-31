029676d62184f4906da24de985f6ffc91e48867e
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-31 12:08:41 -0400

    made uplaodProPic button

---
+++ src/components/uploadProPic.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class uploadProPic extends React.Component {
+
+	state = { 
+		proPicFile: null
+	}
+
+	handleSubmit = (e) => {
+		// debugger;
+		e.preventDefault();
+		const formData = new FormData();
+		formData.append('pro_pic[user_id]', this.props.userInfo.id);
+		formData.append('pro_pic[picture]', this.state.proPicFile);
+		// const formData = { user_id: this.props.userInfo.id, picture: this.state.photoFile};
+		debugger;
+		axios({
+	    method: 'POST',
+	    url: `http://localhost:3000/pro_pics`,
+	    data: formData,
+	    config: { headers: {'Content-Type': 'multipart/form-data', Authorization: localStorage.token }}
+	  });
+		// fetch('http://localhost:3000/photos', {
+		// 	method: 'POST',
+		// 	headers: {
+		// 		'Content-Type': 'application/json',
+		// 		'Accept': 'application/json',
+		// 		'Authorization': localStorage.token
+		// 	},
+		// 	body: JSON.stringify(formData)
+		// }).then(res => res.json())
+		// 	.then(console.log)
+		// $.ajax({
+		// 	url:'/photos',
+		// 	method: 'POST',
+		// 	data: formData,
+		// 	contentType: false,
+		// 	processData: false
+		// })
+	}
+
+	handleFile = (e) => {
+		this.setState({photoFile: e.currentTarget.files[0]});
+	}
+
+	render() {
+		console.log(this.state);
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<input type="file" onChange={this.handleFile}/>
+				<button>Upload Photo </button>
+			</form>
+		);
+	}
+}
