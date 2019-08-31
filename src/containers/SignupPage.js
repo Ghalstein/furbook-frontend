f85cff67c6472a858eafbbb4964a1cd1fc91200c
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-31 11:08:01 -0400

    signup looking good

--- src/containers/SignupPage.js
+++ src/containers/SignupPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class SignupPage extends React.Component{
 
@@ -44,13 +45,26 @@ class SignupPage extends React.Component{
   render = () => {
   	if (localStorage.token) this.props.history.push("/home")
     return (
-      <form className="loginForm" onSubmit={this.handleSubmit}>
-        <input className="loginInput" type="text" value={this.state.username} onChange={this.handleChange} name="username"/>
-        <input className="loginInput" type="text" value={this.state.email} onChange={this.handleChange} name="email"/>
-        <input className="loginInput" type="password" value={this.state.password} onChange={this.handleChange} name="password"/>
-        <input className="loginInput" type="password" value={this.state.passwordConfirmation} onChange={this.handleChange} name="passwordConfirmation"/>
-        <input className="loginSubmit" type="submit" value="Login"/>
-      </form>
+    	<div className="LoginPage">
+	    	<div className="form-container sign-up-container">
+		      <form className="signupForm" onSubmit={this.handleSubmit}>
+						<h1>Create Account</h1>
+			      <input className="signupInput" type="text" placeholder="Name" value={this.state.username} onChange={this.handleChange} name="username"/>
+			      <input className="signupInput" type="text" placeholder="Email" value={this.state.email} onChange={this.handleChange} name="email"/>
+			      <input className="signupInput" type="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} name="password"/>
+			      <input className="signupInput" type="password" placeholder="Confirm Password" value={this.state.passwordConfirmation} onChange={this.handleChange} name="passwordConfirmation"/>
+			      <input className="signupSubmit" type="submit" value="Login"/>
+					</form>
+				</div>
+				<div className="overlay-container">
+					<div className="overlay">
+						<div className="overlay-panel overlay-right">
+							<h1>Welcome Back!</h1><p>To keep connected with us please login with your personal info</p>
+							<Link to="/"><button className="ghost" id="signIn">Sign In</button></Link>
+						</div>
+					</div>
+				</div>
+			</div>
     );
   }
 }
