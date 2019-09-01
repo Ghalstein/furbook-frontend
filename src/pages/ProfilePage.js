250a186bc6587c5e9e0cf46ad950fd3814d58266
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-01 17:04:52 -0400

    profile pages added

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -13,11 +13,12 @@ class ProfilePage extends React.Component {
   componentDidMount = () => {
     if (!localStorage.token && this.props.hasOwnProperty('history')) this.props.history.push("/")
   }
-  
+
   render = () => {
+    // debugger;
     return (
       <div className="ProfilePage">
-        ProfilePage
+        <h1> {this.props.user.username ? `Hello ${this.props.user.username}!` : 'Getting your profile...'}</h1>
         <UploadPhoto userInfo={this.props.userInfo}/>
         <UploadProPic userInfo={this.props.userInfo}/>
       </div>
@@ -25,4 +26,14 @@ class ProfilePage extends React.Component {
   }
 }
 
-export default ProfilePage;
+const mapStateToProps = state => {
+  return {
+    user: state.currentUser
+  }
+}
+
+const mapDispatchToProps = {
+  setCurrentUser: getCurrentUser
+}
+
+export default withAuth(connect(mapStateToProps, mapDispatchToProps)(withRouter(ProfilePage)))
