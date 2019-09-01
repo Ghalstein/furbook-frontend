796eb865fd4af2e5cedd64f583408f9196478f2a
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-01 15:01:34 -0400

    user actions made

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -32,22 +32,27 @@ export const logIn = (username, password) => dispatch => {
 export const signUp = (username, password) => dispatch => {
   dispatch({ type: "SIGNUP_REQUEST_START" })
   return fetch('http://localhost:3000/signup', {
-			method: "POST",
-			headers: {
-				'Content-Type': 'application/json',
-				'Accept': 'application/json'
-			},
-			body: JSON.stringify({
-				username: this.state.username,
-				password: this.state.password,
-				email: this.state.email
-			})
+		method: "POST",
+		headers: {
+			'Content-Type': 'application/json',
+			'Accept': 'application/json'
+		},
+		body: JSON.stringify({
+			username: this.state.username,
+			password: this.state.password,
+			email: this.state.email
 		})
-		.then(res => res.json())
-		.then(resp => {
-			if (resp.token !== undefined) {
-				localStorage.setItem('token', resp.token)
-				this.props.history.push('/home')
-			}
-		});
-	}
+	})
+	.then(res => res.json())
+	.then(resp => {
+		if (resp.token !== undefined) {
+			localStorage.setItem('token', resp.token)
+			this.props.history.push('/home')
+		}
+	});
+}
+
+export const logOut = () => dispatch => {
+  localStorage.clear()
+  dispatch({ type: 'LOGOUT' })
+}
