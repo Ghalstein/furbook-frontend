abafbffce555df6398eb3dc9b201828275fd4fd7
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-01 13:08:58 -0400

    login page modified

--- src/containers/LoginPage.js
+++ src/containers/LoginPage.js
@@ -56,7 +56,7 @@ class LoginPage extends React.Component{
 						</div>
 						<div className="overlay-panel overlay-right">
 							<h1>Hello, Friend!</h1>
-							<p>Enter your personal details and start journey with us</p>
+							<p>Enter your personal details and start your journey with us</p>
 							<Link to="/signup"><button className="ghost" id="signUp">Sign Up</button></Link>
 						</div>
 					</div>
