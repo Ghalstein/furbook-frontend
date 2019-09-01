00ea93ecabe209cca20b930c9fa1ec377313a3e5
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-01 13:09:17 -0400

    index modified

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import App from './App';
 import rootReducer from './reducers/rootReducer.js'
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
-const store = createStore(rootReducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
 	<Provider store={store}>
