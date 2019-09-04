2f7bef5c87b8b43a0cdf93ac3a6d6eea8ccc2b1b
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-04 14:03:02 -0400

    post reducer reducing

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -6,6 +6,11 @@ export default (state = defaultState, action) => {
   // console.log('in messages reducer', action);
 
   switch (action.type) {
+  	case 'FETCH_MY_POSTS':
+      return {
+        ...state,
+        posts: action.payload
+      }
     case 'CREATE_POST':
       return {
       	...state,
