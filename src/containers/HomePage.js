cf43d20db9aa92b08c8042372108b1ddd9024115
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-04 14:02:44 -0400

    home page working

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -3,43 +3,47 @@ import { connect } from 'react-redux';
 import { getCurrentUser } from '../actions/userActions';
 import withAuth from '../hocs/withAuth';
 import { withRouter } from 'react-router-dom';
-import Post from '../components/Post'
+import Post from '../components/Post';
+import CreatePost from '../components/CreatePost';
+import { getPosts } from '../actions/postActions';
 
 class HomePage extends React.Component {
 
-  state = {
-    // posts: this.props.currentUser.posts,
-    postContent: ''
-  }
+  // state = {
+  //   // posts: this.props.currentUser.posts,
+  //   postContent: ''
+  // }
 
-  handleChange = (event) => {
-    this.setState({[event.target.name]: event.target.value})
-  }
+  // handleChange = (event) => {
+  //   this.setState({[event.target.name]: event.target.value})
+  // }
 
-  makePost = (postContent, userId) => {
-    fetch('http://localhost:3000/posts', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-         Authorization: localStorage.token
-      },
-      body: JSON.stringify({
-        user_id: userId,
-        content: postContent
-      })
-    })
-     .then(res => res.json())
-     .then(this.props.history.push("/home"))
-  }
+  // makePost = (postContent, userId) => {
+  //   fetch('http://localhost:3000/posts', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       'Accept': 'application/json',
+  //        Authorization: localStorage.token
+  //     },
+  //     body: JSON.stringify({
+  //       user_id: userId,
+  //       content: postContent
+  //     })
+  //   })
+  //    .then(res => res.json())
+  //    .then(this.props.history.push("/home"))
+  // }
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.makePost(this.state.postContent, this.props.user.id)
-  }
+  // handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   this.makePost(this.state.postContent, this.props.user.id)
+  // }
 
   componentDidMount() {
     if ((!localStorage.token) && this.props.hasOwnProperty('history')) this.props.history.push("/")
+      this.props.getPosts();
+      // console.log(this.props);
   }
 
   render = () => {
@@ -47,11 +51,7 @@ class HomePage extends React.Component {
       <div className="HomePage">
         <h1 className="Hi"> {this.props.user.id ? `Hello ${this.props.user.username}!` : 'Getting your profile...'}</h1>
         <div className="postForm">
-          <form className="postForm" onSubmit={this.handleSubmit}>
-            <h1>Make a post</h1>
-            <textarea className="postInput" type="text" placeholder="What's on your mind?" value={this.state.postContent} onChange={this.handleChange} name="postContent"/>
-            <input className="postSubmit" type="submit" value="Post"/>
-          </form>
+          <CreatePost user={this.props.user}/>
         </div>
         <div className="posts-container">
           <ul className="posts">{this.props.user.id ? this.props.user.posts.map(post => <Post post={post}/>) : 'Getting your feed...'}</ul>
@@ -62,15 +62,15 @@ class HomePage extends React.Component {
 }
 
 const mapStateToProps = state => {
-  console.log(state.currentUser)
+  console.log(state)
   return {
-    user: state.currentUser,
-    posts: state.posts
+    user: state.currentUser
   }
 }
 
 const mapDispatchToProps = {
-  setCurrentUser: getCurrentUser
+  setCurrentUser: getCurrentUser,
+  getPosts: getPosts
 }
 
 export default withAuth(connect(mapStateToProps, mapDispatchToProps)(withRouter(HomePage)))
