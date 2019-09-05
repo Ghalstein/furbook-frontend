d0e948a0f469f62485d72f0cecb9175080fb46c5
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-05 12:46:21 -0400

    comment actions working

--- src/actions/commentActions.js
+++ src/actions/commentActions.js
@@ -22,20 +22,19 @@ export const createComment = (postContent, userId, postId) => {
   }
 }
 
-// export const getComments = () => {
-
-//   return function(dispatch){
-//     fetch("http://localhost:3000/comments", { 
-//       headers: {
-//         'Content-Type': 'application/json',
-//         'Accept': 'application/json',
-//          Authorization: localStorage.token
-//       }})
-//     .then(res => res.json())
-//     .then(comments => {
-//       dispatch({ type: 'FETCH_MY_COMMENTS', payload: comments})
-//     })
-//   }
-//   // Return is an action
-//   // return { type: FETCH_MY_POSTS, payload: myWallPosts }
-// }
+export const getComments = () => {
+  return function(dispatch){
+    fetch("http://localhost:3000/comments", { 
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+         Authorization: localStorage.token
+      }})
+    .then(res => res.json())
+    .then(comments => {
+      dispatch({ type: 'FETCH_MY_COMMENTS', payload: comments})
+    })
+  }
+  // Return is an action
+  // return { type: FETCH_MY_POSTS, payload: myWallPosts }
+}
