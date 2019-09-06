d30eb413f2e6f0dcd89f7720d6b26224f3dbb14c
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-06 07:51:47 -0400

    users reducer finalized

---
+++ src/reducers/usersReducer.js
@@ -0,0 +1,24 @@
+const defaultState = {
+	users: []
+}
+
+export default (state = defaultState, action) => {
+  // console.log('in messages reducer', action);
+
+  switch (action.type) {
+  	case 'FETCH_USERS':
+      return {
+        ...state,
+        users: action.payload
+      }
+    // case 'CREATE_POST':
+    // 	// debugger
+    // 	// let post = {...action.payload.object, comments:[], user:{id: action.payload.object.user_id}}
+    //   return {
+    //   	...state,
+    //     users: [action.payload.object, ...state.users]
+    //   }
+    default:
+      return state
+  }
+}
