76f3ba77ffe200ed421d8d3237a4980c113019b5
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-06 12:45:27 -0400

    pro pic showing

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -12,13 +12,14 @@ class HomeHeader extends React.Component {
 
   render = () => {
   	if (!localStorage.token && this.props.hasOwnProperty('history')) this.props.history.push("/")
+      console.log(this.props)
     return (
       <div className="dropdownmenu">
         <ul>
           <li><h2>furbook</h2></li>
           <li><SearchBar/></li>
           <li><Link to="/home"> Feed</Link></li> 
-        	<li><Link to="/profile"> Profile</Link></li> 
+        	<li><Link to="/profile"> <div className="profile-icon-combo"> { this.props.user.id ? <img className="profile-link-icon" src={`${this.props.user.pro_pic.picture.url}`}/> : null} Profile </div></Link></li> 
           <li onClick={this.onLogout}><Link to="/login">Logout</Link></li>
         </ul>
       </div>
