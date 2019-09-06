1bf43d47619d4d231d7e7da612bc6a62624a03b3
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-06 07:52:30 -0400

    user found is appearing

---
+++ src/components/UserFound.js
@@ -0,0 +1,37 @@
+import React from 'react';
+// import { connect } from 'react-redux';
+// import { getCurrentUser } from '../actions/userActions';
+// import withAuth from '../hocs/withAuth';
+// import { withRouter } from 'react-router-dom';
+// import Post from '../components/Post';
+// import CreatePost from '../components/CreatePost';
+// import { getPosts } from '../actions/postActions';
+
+class UserFound extends React.Component {
+
+
+  render = () => {
+    
+    // console.log("homepage: ", this.props)
+    return (
+      <div className="user-found">
+        {this.props.user}
+      </div>
+    );
+  }
+}
+
+// const mapStateToProps = state => {
+//   // console.log(state)
+//   return {
+//     user: state.currentUser,
+//     posts: state.postReducer.posts
+//   }
+// }
+
+// const mapDispatchToProps = {
+//   setCurrentUser: getCurrentUser,
+//   getPosts: getPosts
+// }
+
+export default UserFound;
