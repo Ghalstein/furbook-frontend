ce8a0dd665429ef5a5a3536081f062894880a2ce
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-08 02:32:23 -0400

    updated

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -4,11 +4,15 @@ import loggedIn from './loggedInReducer';
 import postReducer from './postReducer';
 import commentReducer from './commentReducer';
 import usersReducer from './usersReducer';
+import userReducer from './userReducer';
+import targetPostReducer from './targetPostReducer';
 
 export default combineReducers({
   currentUser,
   loggedIn, 
   postReducer,
   commentReducer,
-  usersReducer
+  usersReducer,
+  userReducer,
+  targetPostReducer
 })
