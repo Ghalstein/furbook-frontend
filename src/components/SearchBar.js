71cb591312ed9f82253a54c6455b35dbe56e6393
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-08 02:28:42 -0400

    updated

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getUsers } from '../actions/usersActions'
 import SearchResults from '../containers/SearchResults';
+import withAuth from '../hocs/withAuth';
+import { withRouter } from 'react-router-dom';
 
 class SearchBar extends React.Component {
 
@@ -14,7 +16,7 @@ class SearchBar extends React.Component {
 
   handleChange = (event) => {
     this.setState({[event.target.name]: event.target.value})
-    let search = this.props.users.map(user => user.username).filter(username => username.toLowerCase().includes(this.state.search.toLowerCase()))
+    let search = this.props.users.filter(user => user.username.toLowerCase().includes(this.state.search.toLowerCase()))
     this.setState({users: search})
   }
 
@@ -67,4 +69,4 @@ const mapDispatchToProps = {
   getUsers: getUsers
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(SearchBar)
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(SearchBar))
