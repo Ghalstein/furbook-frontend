f9564ac4ee76dc14cde4aa7a8ba4ffe20b985b31
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-09 17:22:44 -0400

    prelim dropdownworking

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { logOut } from '../actions/userActions';
 import SearchBar from './SearchBar';
+import Notifications from './Notifications';
 
 class HomeHeader extends React.Component {
 
@@ -13,9 +14,19 @@ class HomeHeader extends React.Component {
   goToProfile = () => {
 
   }
+
+  state = {
+    notificationsClicked: false
+  }
+
+  handleNotifClick = () => {
+    this.setState({notificationsClicked: !this.state.notificationsClicked})
+  }
+
   render = () => {
   	if (!localStorage.token && this.props.hasOwnProperty('history')) this.props.history.push("/")
       // console.log(this.props)
+    // debugger
     return (
       <div className="dropdownmenu">
         <ul>
@@ -23,9 +34,16 @@ class HomeHeader extends React.Component {
           <li><SearchBar/></li>
           <li><Link to="/home">Feed</Link></li> 
         	<li><Link onClick={this.goToProfile} to={`/users/${this.props.user.id}`}>Profile</Link></li> 
-          <li></li>
+          <li><a onClick={this.handleNotifClick}>Notifications</a></li>
           <li onClick={this.onLogout}><Link to="/login">Logout</Link></li>
         </ul>
+        <div className="notifications-modal">
+          {this.state.notificationsClicked ?
+            <Notifications requests={this.props.user.pending_friend_requests}/>
+          :
+            null
+          }
+        </div>
       </div>
     );
   }
