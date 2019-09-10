9b03e06bbdde68d7c8223b7c6904be3cd11ba70a
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-10 17:03:10 -0400

    added message reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -4,11 +4,13 @@ import loggedIn from './loggedInReducer';
 import postReducer from './postReducer';
 import commentReducer from './commentReducer';
 import usersReducer from './usersReducer';
+import messageReducer from './messageReducer';
 
 export default combineReducers({
   currentUser,
   loggedIn, 
   postReducer,
   commentReducer,
-  usersReducer
+  usersReducer,
+  messageReducer
 })
