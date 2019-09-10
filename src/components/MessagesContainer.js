4cac16ba78cebdbca5871c00a779881ae0f7a020
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-10 15:00:28 -0400

    made the messages container component

---
+++ src/components/MessagesContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class MessagesContainer extends React.Component {
+
+	render = () => {
+
+		return (
+			<div>
+			MessagesContainer
+			</div>
+		)
+	}
+}
+
+export default MessagesContainer
