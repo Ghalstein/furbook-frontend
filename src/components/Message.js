aee403ab8b27c421f13b73bc4d0a1afe29d27cc4
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-10 19:13:02 -0400

    messages working

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,74 +2,58 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-class Comment extends React.Component {
+class Message extends React.Component {
 
   state = {
-    messageInfo: {}
+    info: {}
   }
   
   componentDidMount() {
-    // debugger
-    fetch(`http://localhost:3000/users/${this.props.comment.user_id}`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-         Authorization: localStorage.token
-      } 
-    })
-    .then(res => res.json())
-    .then(info => this.setState({user: info}))
+    if (this.props.messageInfo.user_id === this.props.user.id) {
+      this.setState({info: this.props})
+      this.setState({friend: false})
+    }
+    else {
+      this.setState({info: this.props})
+      this.setState({friend: true})
+    }
   }
   render = () => {
-    console.log(this.state)
-    // console.log("COMMENT:", this.props)
-    let date = new Date(this.props.comment.created_at)
-    date = date.toString();
-    date = date.split(' ');
-    date = date[0] + ' ' + date[1] + ' ' + date[2] + ' ' + date[3];
     // debugger
-    if (Object.keys(this.state.user).length) {
-      return (
-        <li className="comment-content views">
-          <div className="comment-content comment-div">
-            <div className="icon-date">
-              <Link className="comment-link" to={`/users/${this.state.user.id}`} >
-                <div className="icon-img-text">
-                  {this.state.user.pro_pic.length ?
-                    <img className="icon-img" src={this.state.user.pro_pic.splice(-1)[0].picture.url} />
-                  :
-                    <img className="icon-img" src='https://image.flaticon.com/icons/png/512/17/17479.png' />
-                  }
-                  <div className="comment-icon"> 
-                    {this.state.user.username}
-                  </div>
-                </div>
-              </Link>
-              <div className="comment-date">
-                {date.toString()}
-              </div>
-            </div>
-            <div className="comment-text">
-              {this.props.comment.content}
-            </div>
+    if (!Object.keys(this.state.info).length) return null
+    if (this.state.info.messageInfo.friendship.id !== this.props.friendship_id) return null;
+    console.log(this.props)
+    // console.log("COMMENT:", this.props)
+    // let date = new Date(this.props.mesageInfo.created_at)
+    // date = date.toString();
+    // date = date.split(' ');
+    // date = date[0] + ' ' + date[1] + ' ' + date[2] + ' ' + date[3];
+    // debugger
+    return (
+      <li className="message-content views">
+        <div className="message-content comment-div">
+          <div className="icon-date">
           </div>
-        </li>
-      );
-    }
-    else return null
+          { this.state.info.messageInfo.user.id !== this.props.user.id ?
+            <div className="friend-message-text">
+              {this.state.info.messageInfo.content}
+            </div>
+          :
+            <div className="user-message-text">
+              {this.state.info.messageInfo.content}
+            </div>
+          }
+        </div>
+      </li>
+    );
   }
 }
 
 const mapStateToProps = state => {
   // console.log(state)
   return {
-    user: state.currentUser,
-    comments: state.commentReducer.comments
+    user: state.currentUser
   }
 }
 
-const mapDispatchToProps = {
-  // more to do for getComments redux
-}
-
-export default connect()(Message);
+export default connect(mapStateToProps)(Message);
