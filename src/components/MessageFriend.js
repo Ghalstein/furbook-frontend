f452ef4ef774da2313a47f144f8457a4c44f247e
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-10 17:42:12 -0400

    added mroe style to messaging

--- src/components/MessageFriend.js
+++ src/components/MessageFriend.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import CreateMessage from './CreateMessage'
+import CreateMessage from './CreateMessage';
+import { getMessages } from '../actions/messageActions';
+import { connect } from 'react-redux';
+import withAuth from '../hocs/withAuth';
+import { withRouter } from 'react-router-dom';
 
 class MessageFriend extends React.Component {
 
 	state = {
 		messageOpend: false
 	}
+
+	componentDidMount() {
+  	this.props.getMessages();
+  }
 
 	handleOpenMessages = () => {
 		this.setState({messageOpend: true})
@@ -18,12 +26,16 @@ class MessageFriend extends React.Component {
 	render = () => {
 		// debugger
 		console.log(this.props.messageInfo)
+		let messages = this.props.messages.filter(message => message.friendship_id === this.props.messageInfo.friendship_id)
 		return (
 				<div>
 					{this.state.messageOpend ?
 						<div className="chat">
 							<div className="top-chat">
 								<a onClick={this.handleCloseMessages} className="close x-out">x</a>
+							</div>
+							<div className="message-friend-name">
+								{this.props.messageInfo.friend.username}
 							</div>
 							<div>
 								<CreateMessage messageInfo={this.props.messageInfo}/>
@@ -39,4 +51,17 @@ class MessageFriend extends React.Component {
 	}
 }
 
-export default MessageFriend;
+const mapStateToProps = state => {
+  // console.log(state)
+  return {
+    user: state.currentUser,
+    messages: state.messageReducer.messages
+  }
+}
+
+const mapDispatchToProps = {
+  // more to do for getComments redux
+  getMessages: getMessages
+}
+
+export default withAuth(connect(mapStateToProps, mapDispatchToProps)(withRouter(MessageFriend)))
