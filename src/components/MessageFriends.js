89e28845b957af03eb33a3cacc813376505ee9ce
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-11 18:55:09 -0400

    message friends working

--- src/components/MessageFriends.js
+++ src/components/MessageFriends.js
@@ -5,7 +5,7 @@ class MessageFriends extends React.Component {
 	render = () => {
 		return (
 			<div className="message-friends">
-				{this.props.messagesInfo.map( messageInfo => <MessageFriend messageInfo={messageInfo}/>)}
+				{this.props.messagesInfo.map( messageInfo => <MessageFriend updateMessage={this.props.updateMessage} messageInfo={messageInfo}/>)}
 			</div>
 		)
 	}
