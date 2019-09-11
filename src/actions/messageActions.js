60a4b503e12face2a0845b80ec448dcf9d238e19
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-11 18:52:57 -0400

    message actions up to date

--- src/actions/messageActions.js
+++ src/actions/messageActions.js
@@ -38,3 +38,28 @@ export const getMessages = () => {
   // Return is an action
   // return { type: FETCH_MY_POSTS, payload: myWallPosts }
 }
+
+export const viewedMessage = (message) => {
+  // debugger
+
+  return function(dispatch) {
+    fetch(`http://localhost:3000/messages/${message.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+         Authorization: localStorage.token
+      },
+      body: JSON.stringify(
+      {
+        'viewed': true
+      })
+    })
+    .then(res => res.json())
+    .then(console.log)
+    // .then(message => {
+    //   // ONCE THE FETCH HAS FINISHED WE SHOULD THEN DISPATCH
+    //   dispatch({type: "VIEWED_MESSAGE", payload: message })
+    // })
+  }
+}
