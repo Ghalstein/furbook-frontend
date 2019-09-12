061e6164f245687b615d711fc26b1b73485b6fc2
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-12 11:09:22 -0400

    up to date

--- src/reducers/currentUserReducer.js
+++ src/reducers/currentUserReducer.js
@@ -6,7 +6,7 @@ const defaultState = {
 
 export default (state = defaultState, action) => {
   // console.log('in root reducer', action);
-
+  // debugger
   switch (action.type) {
     case 'LOGIN_REQUEST_START':
       return {...state, loading: true}
