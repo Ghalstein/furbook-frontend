79abfc7c2c1e6eb4788626b7d5329d4b2fd24a11
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-12 11:08:02 -0400

    up to date

--- src/components/Post.js
+++ src/components/Post.js
@@ -52,8 +52,7 @@ class Post extends React.Component {
     date = date.toString();
     date = date.split(' ');
     date = date[0] + ' ' + date[1] + ' ' + date[2] + ' ' + date[3];
-    if (this.state.postUser === undefined) return null;
-    console.log("hi from posts")
+    // console.log("hi from posts")
     return (
       <li className="post-content">
         <div className="post-div">
