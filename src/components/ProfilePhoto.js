54966c6e703679a17f925126389b3c24e1bf6c9d
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-12 11:08:14 -0400

    up to date

--- src/components/ProfilePhoto.js
+++ src/components/ProfilePhoto.js
@@ -2,10 +2,29 @@ import React from 'react';
 
 export default class ProfilePhoto extends React.Component {
 
+	state = {
+		clicked:false
+	}
+
+	enlarge = () => {
+		this.setState({clicked: !this.state.clicked})
+	}
+
 	render = () => {
 		// debugger
+		// console.log(this.state)
 		return(
-			<img className="profile-photo" src={this.props.photo.picture.url} />
+			<div>
+				{this.state.clicked ? 
+					<div className="pic-modal">
+						<button onClick={this.enlarge} className="closePic">X</button>
+						<img className="modal-profile-photo" src={this.props.photo.picture.url} />
+					</div> 
+				: 
+					null
+				}
+				<img onClick={this.enlarge} className="profile-photo" src={this.props.photo.picture.url} />
+			</div>
 		)
 	}
 }
