34f1dc94d29d27caf71c6a73228feb2147e720b6
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-21 18:23:06 -0400

    refactoring

--- src/components/UserAlert.js
+++ src/components/UserAlert.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import { connect } from 'react-redux';
-// import { getCurrentUser } from '../actions/userActions';
-// import withAuth from '../hocs/withAuth';
-// import { withRouter } from 'react-router-dom';
-// import Post from '../components/Post';
-// import CreatePost from '../components/CreatePost';
-// import { getPosts } from '../actions/postActions';
 
 class UserAlert extends React.Component {
 
@@ -24,18 +17,5 @@ class UserAlert extends React.Component {
     );
   }
 }
-
-// const mapStateToProps = state => {
-//   // console.log(state)
-//   return {
-//     user: state.currentUser,
-//     posts: state.postReducer.posts
-//   }
-// }
-
-// const mapDispatchToProps = {
-//   setCurrentUser: getCurrentUser,
-//   getPosts: getPosts
-// }
 
 export default UserAlert
