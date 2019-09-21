0a25aa0a2701ba39c7d07d3bb022d744e421b18d
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-21 17:47:16 -0400

    refactoring

--- src/components/Message.js
+++ src/components/Message.js
@@ -27,20 +27,11 @@ class Message extends React.Component {
   }
 
   render = () => {
-    // var objDiv = document.querySelector(".dm-container");
-    // if (objDiv) {
-    //   objDiv.scrollTop = objDiv.scrollHeight;
-    // }
-    // debugger
+
     if (!Object.keys(this.state.info).length) return null
     if (this.state.info.messageInfo.friendship.id !== this.props.friendship_id) return null;
     console.log(this.props)
-    // console.log("COMMENT:", this.props)
-    // let date = new Date(this.props.mesageInfo.created_at)
-    // date = date.toString();
-    // date = date.split(' ');
-    // date = date[0] + ' ' + date[1] + ' ' + date[2] + ' ' + date[3];
-    // debugger
+
     return (
       <li className="message-content views">
         <div className="message-content comment-div">
