135953c9102e492a05f9a4c89990c349691ef181
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-21 17:41:10 -0400

    refactoring

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -17,13 +17,6 @@ export default (state = defaultState, action) => {
         ...state, 
         user: action.payload
       }
-    // case 'CREATE_POST':
-    // 	// debugger
-    // 	// let post = {...action.payload.object, comments:[], user:{id: action.payload.object.user_id}}
-    //   return {
-    //   	...state,
-    //     users: [action.payload.object, ...state.users]
-    //   }
     default:
       return state
   }
