f51c4e493d8b71ae042a03b83b60570a7084b95d
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-29 20:27:33 -0400

    touched up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import LoginPage from './LoginPage'
 import SignupPage from './SignupPage'
 import ProfilePage from './ProfilePage'
-
+//index page fot all pages
 export default {
   LoginPage,
   SignupPage,
