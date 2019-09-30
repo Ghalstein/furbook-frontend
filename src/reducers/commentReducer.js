cd5a0566e779fdd40f3bdb4df153a9f46f658bfc
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-29 20:18:32 -0400

    touched up

--- src/reducers/commentReducer.js
+++ src/reducers/commentReducer.js
@@ -6,13 +6,14 @@ export default (state = defaultState, action) => {
   // console.log('in messages reducer', action);
 
   switch (action.type) {
+    // creates a comment
     case 'CREATE_COMMENT':
-      
+      // adds new comment to comments
       return {
       	...state,
         comments: [...state.comments, action.payload]
       }
-      // debugger
+    // gets all comments
     case 'FETCH_MY_COMMENTS':
       // debugger
       return {
