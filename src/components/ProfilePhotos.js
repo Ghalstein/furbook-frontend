e48c6b5fee255c4397e3113e704fd87b304d7629
Garrett Halstein <garrett.halstein@emory.edu>
2019-10-03 13:29:33 -0400

    fixed idnentation bugs

--- src/components/ProfilePhotos.js
+++ src/components/ProfilePhotos.js
@@ -3,11 +3,11 @@ import ProfilePhoto from './ProfilePhoto';
 
 export default class ProfilePhotos extends React.Component {
 
-	render = () => {
-		return(
-			<div className="profile-photos" >
-				{this.props.photos.map(photo => <ProfilePhoto photo={photo}/>)}
-			</div>
-		)
-	}
+  render = () => {
+    return(
+      <div className="profile-photos" >
+        {this.props.photos.map(photo => <ProfilePhoto photo={photo}/>)}
+      </div>
+    )
+  }
 }
