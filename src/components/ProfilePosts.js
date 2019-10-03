e757bbc6bd96cefa4528b94cb628c3c2e8acad66
Garrett Halstein <garrett.halstein@emory.edu>
2019-10-03 13:32:40 -0400

    added the indent fix

--- src/components/ProfilePosts.js
+++ src/components/ProfilePosts.js
@@ -3,11 +3,11 @@ import ProfilePost from './ProfilePost';
 
 export default class ProfilePhotos extends React.Component {
 
-	render = () => {
-		return(
-			<div className="profile-posts" >
-				{this.props.user.reverse_posts.map(post => <ProfilePost post={post} user={this.props.user}/>)}
-			</div>
-		)
-	}
+  render = () => {
+    return(
+      <div className="profile-posts" >
+        {this.props.user.reverse_posts.map(post => <ProfilePost post={post} user={this.props.user}/>)}
+      </div>
+    )
+  }
 }
