9145dbd5c8f61c3569732042a0dc7fe9725c47d8
Garrett Halstein <garrett.halstein@emory.edu>
2019-10-03 13:43:34 -0400

    fixed idnent bugs

--- src/components/uploadProPic.js
+++ src/components/uploadProPic.js
@@ -5,48 +5,48 @@ import { connect } from 'react-redux';
 // simialr to upload photos except for profile pics
 class uploadProPic extends React.Component {
 
-	state = { 
-		proPicFile: null
-	}
+  state = { 
+    proPicFile: null
+  }
 
-	handleSubmit = (e) => {
-		// debugger;
-		e.preventDefault();
-		const formData = new FormData();
-		formData.append('pro_pic[user_id]', this.props.user.id);
-		formData.append('pro_pic[picture]', this.state.proPicFile);
-		// const formData = { user_id: this.props.userInfo.id, picture: this.state.photoFile};
-		axios({
-	    method: 'POST',
-	    url: `https://furbook-api.herokuapp.com/pro_pics`,
-	    data: formData,
-	    config: { headers: {'Content-Type': 'multipart/form-data', 'Authorization': `Bearer ${localStorage.token}` }}
-	  })
-	  .then(() => window.location.reload(false))
-	}
+  handleSubmit = (e) => {
+    // debugger;
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('pro_pic[user_id]', this.props.user.id);
+    formData.append('pro_pic[picture]', this.state.proPicFile);
+    // const formData = { user_id: this.props.userInfo.id, picture: this.state.photoFile};
+    axios({
+      method: 'POST',
+      url: `https://furbook-api.herokuapp.com/pro_pics`,
+      data: formData,
+      config: { headers: {'Content-Type': 'multipart/form-data', 'Authorization': `Bearer ${localStorage.token}` }}
+    })
+    .then(() => window.location.reload(false))
+  }
 
-	handleFile = (e) => {
-		this.setState({proPicFile: e.currentTarget.files[0]});
-	}
+  handleFile = (e) => {
+    this.setState({proPicFile: e.currentTarget.files[0]});
+  }
 
-	render() {
-		// console.log(this.props);
-		return (
-			<div className="uplaod-form">
-				Upload a new profile picture
-				<form onSubmit={this.handleSubmit}>
-					<input type="file" onChange={this.handleFile}/>
-					<button className="upload-button">Upload Photo </button>
-				</form>
-			</div>
-		);
-	}
+  render() {
+  	// console.log(this.props);
+  	return (
+      <div className="uplaod-form">
+        Upload a new profile picture
+        <form onSubmit={this.handleSubmit}>
+          <input type="file" onChange={this.handleFile}/>
+          <button className="upload-button">Upload Photo </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-	return {
-		user: state.currentUser
-	}
+  return {
+    user: state.currentUser
+  }
 }
 
 export default connect(mapStateToProps)(uploadProPic);
