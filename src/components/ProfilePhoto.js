8262e2c0651882fedcdd7d72161d891347de549c
Garrett Halstein <garrett.halstein@emory.edu>
2019-10-03 13:28:42 -0400

    fixed indentation bugs

--- src/components/ProfilePhoto.js
+++ src/components/ProfilePhoto.js
@@ -2,39 +2,39 @@ import React from 'react';
 
 export default class ProfilePhoto extends React.Component {
 
-	state = {
-		clicked:false
-	}
+  state = {
+    clicked:false
+  }
 
-	// enlarges the image
-	enlarge = () => {
-		this.setState({clicked: !this.state.clicked})
-	}
+  // enlarges the image
+  enlarge = () => {
+    this.setState({clicked: !this.state.clicked})
+  }
 
-	leave = (event) => {
-		if (event.target.className === "pic-modal") {
-			this.setState({clicked: false})
-		}
-	}
+  leave = (event) => {
+    if (event.target.className === "pic-modal") {
+      this.setState({clicked: false})
+    }
+  }
 
-	render = () => {
+  render = () => {
 		
-		// conditionally renders a photo or video based on the file its processing
-		return(
-			<div>
-				{this.state.clicked ? 
-					<div className="pic-modal" onClick={this.leave}>
+    // conditionally renders a photo or video based on the file its processing
+    return(
+      <div>
+        {this.state.clicked ? 
+          <div className="pic-modal" onClick={this.leave}>
 							<img className="profile-photo" className="profile-photo" src={this.props.photo.picture.url} />
-					</div> 
-				: 
-					null
-				}
-				{this.props.photo.picture.url.split('.').slice(-1)[0].toLowerCase() === 'mp4' || this.props.photo.picture.url.split('.').slice(-1)[0].toLowerCase() === 'mov' ?
-					<video className="profile-photo" controls src={this.props.photo.picture.url}/>
-				:
-					<img onClick={this.enlarge} className="profile-photo" src={this.props.photo.picture.url} />
-				}
-			</div>
-		)
-	}
+          </div> 
+        : 
+          null
+        }
+        {this.props.photo.picture.url.split('.').slice(-1)[0].toLowerCase() === 'mp4' || this.props.photo.picture.url.split('.').slice(-1)[0].toLowerCase() === 'mov' ?
+          <video className="profile-photo" controls src={this.props.photo.picture.url}/>
+        :
+          <img onClick={this.enlarge} className="profile-photo" src={this.props.photo.picture.url} />
+        }
+      </div>
+    )
+  }
 }
